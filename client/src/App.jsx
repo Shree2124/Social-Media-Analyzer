27d00af6c32390172d4ec600e96480e4ef6aa834
Shree2124 <shreealasande@gmail.com>
2025-12-26 12:33:58 +0530

    feat: vercel configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { Header, HeroSection, Login, Register } from "./components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./App.css";
 
 function App() {
   return (
     <div className="h-full bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
-
-      <Header />
       <BrowserRouter>
         <Routes>
           <Route
@@ -14,17 +14,36 @@ function App() {
             element={
               <>
                 <main className="mx-auto h-full px-4 sm:px-6 lg:px-8 py-8">
+                  <Header />
                   <HeroSection />
                 </main>
               </>
             }
           />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/login"
+            element={
+              <main className="mx-auto h-full px-4 sm:px-6 lg:px-8 py-8">
+                <Header />
+                <Login />
+              </main>
+            }
+          />
 
-          <Route path="/register" element={<Register />} />
-
+          <Route
+            path="/register"
+            element={
+              <main className="mx-auto h-full px-4 sm:px-6 lg:px-8 py-8">
+                <Header />
+                <Register />
+              </main>
+            }
+          />
         </Routes>
       </BrowserRouter>
+
+      <SpeedInsights />
+      <Analytics />
     </div>
   );
 }
